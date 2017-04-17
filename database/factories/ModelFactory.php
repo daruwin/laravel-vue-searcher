af8f9008f12003f330e5ff269efb5573c8d500ba
Daruwin <daruwin@hotmail.com>
2017-04-17 23:58:00 +0100

    several changes

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -21,3 +21,12 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'description' => $faker->paragraph(2),
     ];
 });
+
+$factory->define(App\Product::class, function (Faker\Generator $faker) {
+    return [
+        'title' => $faker->sentence(),
+        'image' => 'http://loremflickr.com/400/300?random='.rand(1, 100),
+        'price' => $faker->numberBetween(3, 100),
+        'description' => $faker->paragraph(2)
+    ];
+});
